f8f42f3bf93d6638874810c8a07e59e596950f37
Thái Việt <thaihv@mintpotvn.com>
2021-10-25 12:01:45 +0700

    update cors

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieparser = require("cookie-parser");
 const productRoutes = require("./src/routes/productRoutes");
 const categoryRoutes = require("./src/routes/cartegoryRoutes");
 const userRoutes = require("./src/routes/userRoutes");
+const cors = require('cors')
 // mysql://bddaf688c4a62a:730c2465@us-cdbr-east-04.cleardb.com/heroku_62ef357af56d406?reconnect=true
 var conn = mysql.createPool({
   host: "us-cdbr-east-04.cleardb.com",
@@ -14,6 +15,12 @@ var conn = mysql.createPool({
   database: "heroku_62ef357af56d406",
 });
 
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 // conn.connect(function (err) {
 //   if (err) console.log("err connect to db");
 //   console.log("connect success");
