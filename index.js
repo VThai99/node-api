a13679003012dc9d36cfaea33642a88eef23e935
Thái Việt <thaihv@mintpotvn.com>
2021-10-25 11:14:41 +0700

    update

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const productRoutes = require("./src/routes/productRoutes");
 const categoryRoutes = require("./src/routes/cartegoryRoutes");
 const userRoutes = require("./src/routes/userRoutes");
 // mysql://bddaf688c4a62a:730c2465@us-cdbr-east-04.cleardb.com/heroku_62ef357af56d406?reconnect=true
-var conn = mysql.createConnection({
+var conn = mysql.createPool({
   host: "us-cdbr-east-04.cleardb.com",
   user: "bddaf688c4a62a",
   password: "730c2465",
   database: "heroku_62ef357af56d406",
 });
 
-conn.connect(function (err) {
-  if (err) console.log("err connect to db");
-  console.log("connect success");
-});
+// conn.connect(function (err) {
+//   if (err) console.log("err connect to db");
+//   console.log("connect success");
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
