db187af5e16397d3503101b24a9faf862ff3ca02
Thái Việt <thaihv@mintpotvn.com>
2021-10-27 10:28:50 +0700

    update

--- src/routes/cartegoryRoutes.js
+++ src/routes/cartegoryRoutes.js
@@ -6,11 +6,7 @@ const fileUploader = require("../common/cloudy/cloudinary");
 router.get("/list", controller.getAllCategory);
 router.get("/:id", controller.getProductInCate);
 router.post("/add", fileUploader.single("image"), controller.createCategory);
-router.put(
-  "/update/:id",
-  fileUploader.single("image"),
-  controller.updateCategory
-);
+router.put("/update/:id",fileUploader.single("image"),controller.updateCategory);
 router.delete("/delete/:id", controller.deleteCategory);
 
 module.exports = router;
